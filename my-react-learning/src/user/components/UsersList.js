aeadda9b3c29def5436685afca1234b8d1c8e5c1
Hao <aszqsc@gmail.com>
2025-12-11 00:31:45 +0700

    Learn video 5.7

---
+++ my-react-learning/src/user/components/UsersList.js
@@ -0,0 +1,24 @@
+import "./UsersList.css";
+
+import UserItem from "./UserItem";
+
+const UsersList = (props) => {
+  if (!props.items || props.items.length === 0) {
+    return <div className="center">No users found.</div>;
+  }
+
+  return (
+    <ul className="users-list">
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.placeCount}
+        ></UserItem>
+      ))}
+    </ul>
+  );
+};
+export default UsersList;
--- my-react-learning/src/user/pages/Users.js
+++ my-react-learning/src/user/pages/Users.js
@@ -1,5 +1,17 @@
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Schwarz",
+      image:
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      placeCount: 3,
+    },
+  ];
+
+  return <UsersList items={USERS}></UsersList>;
 };
 
 export default Users;
